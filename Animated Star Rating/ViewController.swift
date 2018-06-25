238356eb82ce30b8a331578a6bffd82bcdc0799d
MuhammadUmar <umar@devicebee.com>
2018-06-25 10:43:42 +0500

    Initial Commit

---
+++ Animated Star Rating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animated Star Rating
+//
+//  Created by Muhammad Umar on 25/06/2018.
+//  Copyright © 2018 Neberox Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Animated Star RatingTests/Animated_Star_RatingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Animated_Star_RatingTests.swift
+//  Animated Star RatingTests
+//
+//  Created by Muhammad Umar on 25/06/2018.
+//  Copyright © 2018 Neberox Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import Animated_Star_Rating
+
+class Animated_Star_RatingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
